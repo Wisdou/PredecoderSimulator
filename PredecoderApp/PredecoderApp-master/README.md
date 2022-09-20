1ae85fd6713f64b600956539acad6eaafef2ef10
Wisdou <51473681+Wisdou@users.noreply.github.com>
2022-09-20 20:51:02 +0300

    Add initial files

---
+++ PredecoderApp/PredecoderApp-master/README.md
@@ -0,0 +1,9 @@
+# Предекодер
+
+## Описание
+Симулятор предекодера ядра CPU, способный декодировать команды ассемблеров MIPS и MASM.
+
+## Справочная информация
+- [Предекодер](https://wiki.miem.hse.ru/ru/Projects/516)
+- [MASM](https://ru.wikipedia.org/wiki/MASM)
+- [MIPS](https://ru.wikipedia.org/wiki/MIPS)
---
+++ PredecoderWeb/PredecoderWebApplication/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+function redirectOnClick("") {
+    document.location = 'ControllerName/ActionName';
+}
+// Write your JavaScript code.
